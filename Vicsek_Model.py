c8408b7170e2cf2b79f15c2f5ae51258b0227fa8
sofiraponi <sofia.raponi@studio.unibo.it>
2022-10-23 00:51:51 +0200

    Add sixth version of Vicsek file: neighbors mean angle function

--- Vicsek_Model.py
+++ Vicsek_Model.py
@@ -61,7 +61,41 @@ def VelocityUpdate(v0,theta):
 
     vel=np.array([vx,vy])
 
-    return vx, vy
+    return vel
+
+def NeighborsMeanAngle(config,N,R0):
+
+    """
+
+    This function calculates the mean orientation of the particles within the intarction radius R0
+
+    Parameters
+        config: previous particles configuration
+        vel: particles velocity
+        N: number of particles
+        R0: interaction radius
+
+    Returns:
+        Updated configuration.
+
+    """
+
+    mean_theta=np.empty(N)
+
+    for i in range(0,N):
+
+        neighbors=[]
+
+        for j in range(0,N):
+
+            if (config[0][i] - config[0][j])**2 + (config[1][i] - config[1][j])**2 <= R0**2:
+                neighbors.append(j)
+
+        mean_cos = np.mean(np.cos(config[2][neighbors]))
+        mean_sin = np.mean(np.sin(config[2][neighbors]))
+        mean_theta[i] = np.arctan2(mean_sin, mean_cos)
+
+    return mean_theta
 
 
 def ConfigurationUpdate(config,vel,R0,eta,N,L,dt):
@@ -93,20 +127,8 @@ def ConfigurationUpdate(config,vel,R0,eta,N,L,dt):
     config[1] = config[1] % L
 
     # Calculate the mean orientation of particles within R0
-    mean_theta=np.empty(N)
 
-    for i in range(0,N):
-
-        neighbors=[]
-
-        for j in range(0,N):
-
-            if (config[0][i] - config[0][j])**2 + (config[1][i] - config[1][j])**2 <= R0**2:
-                neighbors.append(j)
-
-        mean_cos = np.mean(np.cos(config[2][neighbors]))
-        mean_sin = np.mean(np.sin(config[2][neighbors]))
-        mean_theta[i] = np.arctan2(mean_sin, mean_cos)
+    mean_theta =  NeighborsMeanAngle(config,N,R0)
 
     # Update particles orientation
     config[2] = mean_theta + eta*(np.random.rand(N)-0.5)
