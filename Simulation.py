8704177d33079b26f37be715d77a1177c9e87a81
sofiraponi <sofia.raponi@studio.unibo.it>
2022-10-22 19:52:34 +0200

    Add third version of Simulation file: comments

--- Simulation.py
+++ Simulation.py
@@ -1,12 +1,11 @@
-#==============================================================
-#Author: Sofia Raponi
-#Date: 06 October, 2022
+#=======================================================================
+# Author: Sofia Raponi
+# Date: 06 October, 2022
 #
 # Simulation
 #
-#  Aim: To simulate Viscek Model for 2 dimensions.
-#
-#==============================================================
+# Aim: To simulate the 2D Viscek Model.
+#=======================================================================
 
 import configparser
 import numpy as np
@@ -14,58 +13,58 @@ import matplotlib.pyplot as plt
 import Vicsek_Model
 
 # Read configuration file
-
 config=configparser.ConfigParser()
 config.read('settings.ini')
 
-# Model parameters
-
-v0 = float(config['parameters']['v0'])    # Constant velocity of particles
-eta = float(config['parameters']['eta'])  # Constant noise amplitude [0,1]
+# Import model parameters
+v0 = float(config['parameters']['v0'])    # Velocity modulus
+eta = float(config['parameters']['eta'])  # Noise amplitude
 R0 = float(config['parameters']['R0'])    # Interaction radius
 dt = float(config['parameters']['dt'])    # Time step
 N = int(config['parameters']['N'])        # Number of particles
-L = int(config['parameters']['L'])        # Dimension of system
+L = int(config['parameters']['L'])        # Dimension of space
 T = int(config['parameters']['T'])        # Number of steps
 
+# Import local paths
 phi_path = config['paths']['phi']
-pos_path = config['paths']['pos']
+position_path = config['paths']['position']
 theta_path = config['paths']['theta']
 
-phi=np.empty(T+1)
-data_pos=np.empty([T+1,2,N])
-data_theta=np.empty([T+1,1,N])
+# Define data arrays
+phi_data=np.empty(T+1)
+position_data=np.empty([T+1,2,N])
+theta_data=np.empty([T+1,1,N])
 
-# Initial configuration
-
+# Calculate initial configuration (position and orientation)
 config = Vicsek_Model.InitialConfiguration(N,L)
 
-# Initial velocity
-
+# Calculate initial velocity
 vel = Vicsek_Model.VelocityUpdate(v0,config[2])
 
-data_pos[0] = config[0], config[1]
-data_theta[0] = config[2]
+# Save initial configuration
+position_data[0] = config[0], config[1]
+theta_data[0] = config[2]
 
-# Initial order parameter
-
-phi[0] = Vicsek_Model.OrderParameter(config[2],N)
+# Calculate initial order parameter
+phi_data[0] = Vicsek_Model.OrderParameter(config[2],N)
 
 # Main loop
-
 for i in range(T):
 
+    # Update configuration
     config = Vicsek_Model.ConfigurationUpdate(config,vel,R0,eta,N,L,dt)
 
+    # Update velocity
     vel = Vicsek_Model.VelocityUpdate(v0,config[2])
 
-    phi[i+1]=Vicsek_Model.OrderParameter(config[2],N)
+    # Calculate order parameter
+    phi_data[i+1]=Vicsek_Model.OrderParameter(config[2],N)
 
-    data_pos[i+1] = config[0], config[1]
-    data_theta[i+1]= config[2]
+    # Save current configuration
+    position_data[i+1] = config[0], config[1]
+    theta_data[i+1]= config[2]
 
-# Save particles confoguration and order parameter evolution
-
-np.save(pos_path,data_pos)
-np.save(theta_path,data_theta)
-np.save(phi_path,phi)
+# Save particles configuration and order parameter evolution
+np.save(position_path,position_data)
+np.save(theta_path,theta_data)
+np.save(phi_path,phi_data)
