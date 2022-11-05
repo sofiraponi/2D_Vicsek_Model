f0ca532af310777d34d77b350da958a000fe179e
sofiraponi <sofia.raponi@studio.unibo.it>
2022-11-05 23:28:34 +0100

    Simulation file correction: documentation

--- Simulation.py
+++ Simulation.py
@@ -45,7 +45,7 @@ vel = Vicsek_Model.VelocityCalculation(v0,config[2])
 # Update particles configuration Ns times
 position_data, theta_data = Vicsek_Model.Simulate(config,vel,R0,eta,L,dt,Ns,v0)
 
-# Calculate the order parameter per each configuration
+# Calculate the order parameter for each configuration
 phi_data=np.empty(Ns+1)
 for i in range(0,len(theta_data)):
     phi = Vicsek_Model.OrderParameter(theta_data[i])
